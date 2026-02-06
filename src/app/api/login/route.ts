938dd70ca49d2bfee8bda6add4028116bb6a661d
sirikotampan111-jpg <sirikotampan111@gmail.com>
2026-02-06 11:42:24 +0700

    Update route.ts

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,1 +1,14 @@
 
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+  const { email, password } = await req.json();
+
+  if (email === 'admin@webku.com' && password === '123456') {
+    const res = NextResponse.json({ success: true });
+    res.cookies.set('login', 'true', { httpOnly: true, path: '/', maxAge: 60*60*24 });
+    return res;
+  }
+
+  return NextResponse.json({ success: false, message: 'Email atau password salah' }, { status: 401 });
+}
