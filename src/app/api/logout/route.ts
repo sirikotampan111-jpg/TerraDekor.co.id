5f7847687070652ffa0543f2e7d77dbfe250fe34
sirikotampan111-jpg <sirikotampan111@gmail.com>
2026-02-06 11:51:51 +0700

    Delete src/app/api directory

--- src/app/api/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST() {
-  const res = NextResponse.json({ success: true });
-  res.cookies.delete('login', { path: '/' });
-  return res;
-}
